39caaad1634ca53c46dd36ee8f9960af8eb4aa3a
jermill graham <jermill@soari.ai>
2025-11-15 08:22:38 -0500

    Update Pump.fun chart link to new  contract address
    - Updated PUMP_URL to new token: BHGKB4j2BfaUyVMKsZUZk4igjFxRHxtU1kZz5gRGpump
    - Updated CONTRACT_ADDRESS constant
    - Lil Qrum image now links to current  chart
    - All CTA buttons point to live trading page

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -6,9 +6,9 @@ export const SOCIAL_LINKS = {
   instagram: 'https://instagram.com/breadqrums',
 };
 
-export const PUMP_URL = 'https://pump.fun/coin/9pLdfxua1tyx9kieuVtJXTsBV2o52ozg7S4Rzjjfpump';
+export const PUMP_URL = 'https://pump.fun/coin/BHGKB4j2BfaUyVMKsZUZk4igjFxRHxtU1kZz5gRGpump';
 
-export const CONTRACT_ADDRESS = '9pLdfxua1tyx9kieuVtJXTsBV2o52ozg7S4Rzjjfpump';
+export const CONTRACT_ADDRESS = 'BHGKB4j2BfaUyVMKsZUZk4igjFxRHxtU1kZz5gRGpump';
 
 export const TOKENOMICS = [
   { name: 'Fair Launch Pool', percentage: 90, description: 'Available for all Breadheads' },
